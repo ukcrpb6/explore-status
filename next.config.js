f30ac2f1111250e37a9b0dc0cd11023056147ae4
Bob Browning <bob.browning@pamediagroup.com>
2021-11-16 09:44:57 +0000

    chore: initial dark mode

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+      'explore.pa.media'
+    ]
+  }
+}
+
+module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <main className="flex flex-col justify-center max-w-screen-xl min-h-screen mx-auto bg-no-repeat bg-daily">
         <div className="min-h-screen py-6 sm:py-12">
           <div className="relative py-12 sm:max-w-xl sm:mx-auto">
-            <div className="relative z-10 px-4 py-12 bg-white border border-gray-200 shadow-lg sm:p-20 sm:pt-12 sm:rounded-3xl backdrop-blur-md bg-opacity-60">
+            <div className="relative z-10 px-4 py-12 bg-white border-b border-r border-gray-200 shadow-lg sm:p-20 sm:pt-12 sm:rounded-3xl backdrop-blur-md bg-opacity-30 dark:bg-black dark:bg-opacity-60 dark:border-gray-700 dark:text-white">
               <div className="">
                 <img
                   src="https://explore.pa.media/images/pa-media-explore-logo.svg"
@@ -32,8 +32,7 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <div className="max-w-md py-6 mx-auto border-t border-b border-gray-400">
-
+              <div className="max-w-md py-6 mx-auto border-t border-b border-gray-400 dark:border-gray-400">
                 <ul>
                   {checks.map((check, index) => (
                     <li key={index} className="mt-4 first:mt-0">
@@ -42,7 +41,7 @@ export default function Home() {
                   ))}
                 </ul>
               </div>
-              <div className="absolute text-xs text-gray-500 bottom-1 right-6">
+              <div className="absolute text-xs text-gray-500 bottom-1 right-6 dark:text-gray-400">
                 <a href="https://nextjs.org/" target="_blank">
                   Powered by Next.js
                 </a>
